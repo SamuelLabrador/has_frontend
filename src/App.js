a0c9d5dceb6eaa7f7aa3a7f29f0a44cd64bbc814
Winson Bi <wbi002@ucr.edu>
2020-02-07 16:23:19 -0800

    Issue with map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, createRef} from 'react';
 import './App.css';
+import Map from './map'
 
+//Map functionality
+class GoogleMap extends Component{
+  render(){
+    return(
+      <Map
+        id="myMap"
+        options={{
+          center: { lat: 41.0082, lng: 28.9784 },
+          zoom: 8
+        }}
+        onMapLoad={map => {
+          var marker = new window.google.maps.Marker({
+            position: { lat: 41.0082, lng: 28.9784 },
+            map: map,
+            title: 'Hello Istanbul!'
+          });
+        }}
+      />
+    );
+  }
+}
+//Map
+
+const Header = () => {
+  return(
+    <div>
+      <h1>Highway Analytics System</h1>
+      <button type="button">Analytics</button>
+      <button type="button">View Cameras</button>
+      <button type="button">Login</button>
+    </div>
+  );
+};
+class MainWindow extends Component{
+  constructor(props){
+    super(props);
+    //initial state
+    this.state={
+
+    }
+  }
+  render(){
+    return(
+      <div>
+        <GoogleMap/>
+      </div>
+    );
+  }
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <MainWindow/>
     </div>
   );
 }
