75ae85ff268bae5dbc46d442926e443f57f530d1
Winson Bi <wbi002@ucr.edu>
2020-02-12 10:20:28 -0800

    Re-Centered map to rive rside

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 //import Marker from './marker/marker';
 //might need to make camera size dynamic as we zoom in and out
+
 const AnyReactComponent = (props: any) => <img src={require('./camera_icon.png')} alt="**HAS CAMERA ICON HERE**" style={{height: '40px'}}/>;
 
 const handleApiLoaded = (map, maps) => {
@@ -10,7 +11,7 @@ const handleApiLoaded = (map, maps) => {
 const GoogleMap = (props: any) => {
   //TODO: Implement user current location detection
   //default lat lng
-  const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
+  const [center, setCenter] = useState({lat: 33.980530, lng: -117.377020 });
   //default zoom 11
   const [zoom, setZoom] = useState(11);
   /* Change the map to be dynamic*/
@@ -25,13 +26,18 @@ const GoogleMap = (props: any) => {
     >
         {/*Markers can be specified here*/}
         <AnyReactComponent  
-          lat={11.0168}
-          lng={76.9558}
+          lat={33.980530}
+          lng={-117.377020}
         />
         <AnyReactComponent  
-          lat={11.0168}
-          lng={76.9558}
+          lat={33.965018}
+          lng={-117.377020}
         />
+        <AnyReactComponent  
+          lat={33.968214}
+          lng={-117.339799}
+          
+        />        
       </GoogleMapReact>
     </div>
   );
