6a1f8d8c860f103e75c4c2158f424f5ca881af7a
SamuelLabrador <sam.labrador@yahoo.com>
2020-02-19 13:11:37 -0800

    fixed issue with map test. Bad library import

--- src/components/map/__test__/map.test.js
+++ src/components/map/__test__/map.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import GoogleMap from './../map';
+import GoogleMap from 'google-map-react';
 afterEach(cleanup);
 
 test('Map Renders', () => {
