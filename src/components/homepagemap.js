e3a185184a48cc92e080c3e099fd0ff692c30b35
Takbir Islam <tisla002@ucr.edu>
2020-03-07 19:12:24 -0800

    Changed Homepagemap from using google-map-react to google-maps-react in order to allow polylines to be built on the homepage map instead.

--- src/components/homepagemap.js
+++ src/components/homepagemap.js
@@ -1,58 +1,17 @@
-import React  from 'react';
-import GoogleMapReact from 'google-map-react';
-// import Marker from './marker.js';
+import React, { Component } from 'react';
+import {Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 
-//reformat
-const InfoWindow = (props: any) => (
-  (props.show && props.showInfoIndex === props.id) ? (
-  <div
-    style={
-      {background:"white",
-      border: "5px solid white",
-      borderRadius: 20,
-      width: 400,
-      height: 250}
-    }>
-    <img
-      //change
-      src = {props.image_url}
-      style={{
-        width: '380px'
-      }}
-    />
-    <div>Lat: {props.lat}  Long: {props.lng}</div>
-    <div> Route: {props.route} </div>
-  </div>) : null
-)
-
-const Marker = (props: any) => (
-  <React.Fragment>
-    <InfoWindow
-      image_url = {props.image_url}
-      lat = {props.lat}
-      lng = {props.lng}
-      id = {props.id}
-      route = {props.route}
-      showInfoIndex = {props.showInfoIndex}
-      show={props.show}
-    />
-    <img
-      src='camera_icon.png'
-      alt="**HAS CAMERA ICON**"
-      style={{height: '40px'}}
-    />
-  </React.Fragment>
-)
-
-
-
-class HomepageMap extends React.Component{
+class HomepageMap extends Component{
   static defaultProps = {
     center: {
         lat: 33.980530,
         lng: -117.377020
     },
-    zoom: 11
+    zoom: 11,
+    style: {
+      width: '100%',
+      height: '100%'
+    }
   };
 
   constructor(props){
@@ -64,9 +23,14 @@ class HomepageMap extends React.Component{
         lat: 33.980530,
         lng: -117.377020
       },
+      style: {
+        width: '100%',
+        height: '100%'
+      },
       zoom: 11,
-      showInfoIndex : null,
-      show: false
+      showingInfoWindow : false,
+      selectedPlace: {},
+      activeMarker: {},
     };
   }
 
@@ -76,7 +40,7 @@ class HomepageMap extends React.Component{
     .then(res => res.json())
     .then(
     (result) => {
-    	console.log(result);
+    	//console.log(result);
     	var list = [];
     	for(var i = 0; i < result.length; i++){
       		var cctv = result[i];
@@ -84,14 +48,14 @@ class HomepageMap extends React.Component{
       			list.push(cctv);
       		}
       	}
-      	console.log(list);
+      	//console.log(list);
       	this.setState({
 	        cctvs: list,
 	        error: false
 	    });
     },
     (error) => {
-        console.log(error);
+        //console.log(error);
         this.setState({
         	ccvts: [],
             error: true
@@ -99,54 +63,102 @@ class HomepageMap extends React.Component{
     });
   }
 
-  _onChildClick = (key, childProps) => {
-    console.log(childProps.id,childProps.lat,childProps.lng)
+  onMarkerClick = (props, marker) => {
     this.setState({
-      show: !this.state.show,
-      showInfoIndex : childProps.id
+      activeMarker: marker,
+      selectedPlace: props,
+      showingInfoWindow: true
     })
-    console.log(this.state.showInfoIndex,this.state.show)
-  }
+    console.log(this.state.showingInfoWindow)
+    //console.log(this.state.selectedPlace)
+  };
 
-  _onChildMouseEnter = (key, childProps) => {
-    //make hover
-  }
+  onMouseoverMarker= (props, marker, e) => {
+    // this.setState({
+    //   activeMarker: marker,
+    //   selectedPlace: props,
+    //   showingInfoWindow: true
+    // })
+    // console.log(this.state.showingInfoWindow)
+  };
 
-  _onChildMouseLeave = (key, childProps) => {
-    //dont hover
-  }
+  onMouseoutMarker= (props, marker, e) => {
+    // this.setState({
+    //   activeMarker: null,
+    //   showingInfoWindow: false
+    // })
+    //console.log(this.state.showingInfoWindow)
+  };
+    
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false
+    });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow){
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      })
+      console.log(this.state.showingInfoWindow)
+    }   
+  };
 
   render(){
-    //TODO: Implement user current location detection
+
+    var icon_image = process.env.PUBLIC_URL + '/camera_icon2.png';
+
     var cctvs = this.state.cctvs.map(
       (d) =>
         <Marker
-          id={d.id}
-          lat={d.latitude}
-          lng={d.longitude}
-          image_url ={d.image_url}
+          icon={icon_image}
+          name = {d.id}
+          onClick = {this.onMarkerClick}
+          onMouseover={this.onMouseoverMarker}
+          onMouseout={this.onMouseoutMarker}
+          position = { {lat: d.latitude, lng: d.longitude} }
+          lat = {d.latitude}
+          long = {d.longitude}
+          image_url = {d.image_url}
           route = {d.route}
-          show={this.state.show}
-          showInfoIndex={this.state.showInfoIndex}
         />
     );
-    console.log(process.env.REACT_APP_GOOGLE_MAP_KEY);
 
     return(
-      <div style={{ height: '80vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:process.env.REACT_APP_GOOGLE_MAP_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          onChildClick={this._onChildClick}
-          // onChildMouseEnter={this._onChildMouseEnter}
-          // onChildMouseLeave={this._onChildMouseLeave}
+      <div style={{ height: '92vh', width: '100%' }}>
+        <Map
+          google={this.props.google}
+          zoom={this.props.zoom}
+          style={this.props.style}
+          initialCenter={this.props.center}
+          onClick={this.onMapClicked}
         >
           {cctvs}
-        </GoogleMapReact>
+          <InfoWindow
+            marker = {this.state.activeMarker}
+            onClose = {this.onInfoWindowClose}
+            visible = {this.state.showingInfoWindow}
+          >
+            <img
+              src = {this.state.selectedPlace.image_url}
+              style={{
+                width: '380px'
+              }}
+            />
+            <div>
+              <span>Lat : {this.state.selectedPlace.lat} Long: {this.state.selectedPlace.long}</span>
+              <p>Route : {this.state.selectedPlace.route}</p>
+            </div>
+          </InfoWindow>
+          
+        </Map>
       </div>
     );
   }
 }
 
-export default HomepageMap;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_KEY
+})(HomepageMap);
