04082954c2bdef0a92e0814ac02ed435a65b0158
Takbir Islam <tisla002@ucr.edu>
2020-03-16 15:55:16 -0700

    Added a vehicles per CCTV table.

--- src/components/AnalyticsGraph.js
+++ src/components/AnalyticsGraph.js
@@ -11,6 +11,8 @@ class AnalyticsGraph extends Component{
       routeVehicle : [],
       value : false,
       series : [],
+      cctvs: [],
+      vehiclesPerCCTV: [],
       activeRoute : 0
     };
     this.getrouteCount()
@@ -57,11 +59,57 @@ class AnalyticsGraph extends Component{
     }); 
   }
 
+  getCCTV(){
+    var url = "http://highwayanalytics.us/api/cctv?format=json&county=Riverside,San+Bernardino";
+    fetch(url)
+    .then(res => res.json())
+    .then(
+    (result) => {
+    	var list = [];
+    	for(var i = 0; i < result.length; i++){
+      		var cctv = result[i];
+      		if(cctv.image_url !== "Not Reported"){
+            list.push(cctv.id);
+      		}
+      	}
+      	this.setState({
+	        cctvs: list,
+	        error: false
+	    });
+    },
+    (error) => {
+        //console.log(error);
+        this.setState({
+        	cctvs: [],
+          error: true
+        })
+    });
+  }
+
+  getVehiclesPerCCTV(){
+    var url = "http://highwayanalytics.us/api/vehiclesPerCCTV"
+
+    fetch(url)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        //console.log(result)
+        this.setState({
+          vehiclesPerCCTV : result,
+        });
+      }, (error) => {
+          console.log("ERROR FETCHING VEHICLE PER CCTV API");
+    });
+  }
+
   componentDidMount() {
-    this.getrouteCount_2()
+    //this.getCCTV();
+    this.getrouteCount_2();
+    this.getVehiclesPerCCTV();
     setInterval(async () => {
       //this.getrouteCount_2();
       this.getrouteCount();
+      this.getVehiclesPerCCTV();
     }, 900000);
   }
 
@@ -136,6 +184,77 @@ class AnalyticsGraph extends Component{
     );
   }
 
+  renderTable2(){
+    var vehiclesPerCCTV = this.state.vehiclesPerCCTV;
+    var sortable = [];
+    var cctvId = []
+    var count = []
+
+    for (var vehicle in vehiclesPerCCTV) {
+        sortable.push([vehicle, vehiclesPerCCTV[vehicle]]);
+    }
+
+    sortable.sort(function(a, b){
+      return a[1] - b[1];
+    });
+
+    sortable.reverse();
+    
+    sortable.forEach(function(item) {
+      cctvId.push(item[0]);
+      count.push(item[1]);
+    })
+
+    return(
+      <div style={{ marginLeft : '40px', marginTop : '40px'}}>
+            <table className="table table-bordered table-hover" id="myTable">
+              <thead>
+                  <tr>
+                      <th>Ranking</th>
+                      <th>CCTV ID</th>
+                      <th>Vehicles</th>
+                  </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>{cctvId[0]}</td>
+                  <td>{count[0]}</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>2</td>
+                  <td>{cctvId[1]}</td>
+                  <td>{count[1]}</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>3</td>
+                  <td>{cctvId[2]}</td>
+                  <td>{count[2]}</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>4</td>
+                  <td>{cctvId[3]}</td>
+                  <td>{count[3]}</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>5</td>
+                  <td>{cctvId[4]}</td>
+                  <td>{count[4]}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+    );
+  }
+
   _updateButtonClicked(){
     var count = this.state.activeRoute
     count += 1;
@@ -156,7 +275,8 @@ class AnalyticsGraph extends Component{
     var series = this.state.series;
     var activeRoute = this.state.activeRoute;
 
-    console.log(series)
+    var vehiclesPerCCTV = this.renderTable2();
+
 
     var routeVehicle = this.state.routeVehicle;
     var data = []
@@ -247,7 +367,8 @@ class AnalyticsGraph extends Component{
           </button>
         </div>
         <div className="row">
-          <div style={{ marginLeft : '40px'}}>
+          {vehiclesPerCCTV}
+          <div style={{ marginLeft : '60px', marginTop : '40px'}}>
             <RadialChart
               colorType='literal'
               getAngle={d => d.count}
@@ -263,7 +384,7 @@ class AnalyticsGraph extends Component{
               {value !== false && <Hint value={value} />}
             </RadialChart>
           </div>
-          <div style={{ marginLeft : '20px'}}>
+          <div style={{ marginLeft : '20px', marginTop : '40px'}}>
             <DiscreteColorLegend 
               height={300} 
               width={200} 
