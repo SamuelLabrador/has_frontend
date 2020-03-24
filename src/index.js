0fc67b4d62f796922d8b6dd164edc4374440a977
SamuelLabrador <sam.labrador@yahoo.com>
2020-03-24 11:47:34 -0700

    adding necessary packages for analytics

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+
+var history = createBrowserHistory();
+ReactGA.initialize('UA-161761065-1');
+
+history.listen((location) => {
+    window.ga('set', 'page', location.pathname + location.search);
+    window.ga('send', 'pageview');
+});
+
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter history={history}>
 		<App />
 	</BrowserRouter>, 
 	document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
